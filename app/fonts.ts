4af30720bb7e9a681c55548676cbf59c2ef0600d
daniel <5633757+dealvz@users.noreply.github.com>
2025-09-30 16:51:03 -0600

    Initial commit - project setup Add global styles, fonts, some deps
    - Add  font styles: Geist and Lora.
    - Updated global CSS variables for theming.
    - Created a components configuration file for ShadCN.
    - Cleaned up the main page layout and removed boilerplate code.

---
+++ app/fonts.ts
@@ -0,0 +1,5 @@
+import { Geist, Geist_Mono, Lora } from "next/font/google";
+
+export const geist = Geist({ variable: "--font-sans", subsets: ["latin"] });
+export const geistMono = Geist_Mono({ variable: "--font-mono", subsets: ["latin"] });
+export const lora = Lora({ variable: "--font-serif", subsets: ["latin"] });
